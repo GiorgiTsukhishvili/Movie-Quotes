9c2d5a808f7e1ba125eaa779643adc5c37703248
giorgi <gcuxishvili@gmail.com>
2022-11-17 11:44:07 +0400

    fix: add break words

--- resources/views/components/movie/movie-quote.blade.php
+++ resources/views/components/movie/movie-quote.blade.php
@@ -3,7 +3,8 @@
 <div class="mb-[67px] border border-black rounded-lx">
     <img src="{{ asset('storage/' . $photo) }}" alt="placeholder"
         class="w-187.5 h-[386px] rounded-t-lx border-b border-black">
-    <h1 class="w-187.5 rounded-b-lx pt-9 pb-11 text-4xl px-[18px] bg-white text-black leading-10 font-normal">
+    <h1
+        class="w-187.5 break-words rounded-b-lx pt-9 pb-11 text-4xl px-[18px] bg-white text-black leading-10 font-normal">
         {{ $quote }}
     </h1>
 </div>
